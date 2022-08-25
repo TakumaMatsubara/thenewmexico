3248785906f9ae307eaab9c249cf468f24aaf2bb
matsubara-19082 <sd19082@tti-j.net>
2022-08-25 20:20:26 +0900

    controllerを追加

---
+++ src/main/kotlin/abc/tutorials/springboot/thenewmexico/controller/BankController.kt
@@ -0,0 +1,15 @@
+package abc.tutorials.springboot.thenewmexico.controller
+
+import abc.tutorials.springboot.thenewmexico.model.Bank
+import abc.tutorials.springboot.thenewmexico.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+}
